90207f05228047c944eaf7769bdc36da96edf275
Toon Schoenmakers <nighteyes1993@gmail.com>
2013-11-12 21:37:15 +0100

    Added a bit more correctness and some preprocessor goodness
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- main.c
+++ main.c
@@ -22,6 +22,9 @@
 #include <stdlib.h>
 #include <endian.h>
 
+#define SECTOR_BYTES 4096
+#define SECTOR_INTS 1024
+
 int main(int argc, char** argv) {
   if (argc < 2) {
     fprintf(stderr, "Requires at least 1 argument!\n");
@@ -29,49 +32,39 @@ int main(int argc, char** argv) {
   }
   FILE* region = fopen(argv[1], "rb+");
   if (region) {
-    uint32_t offsets[1024];
-    if (fread(offsets, 4, 1024, region) != 1024)
+    uint32_t offsets[SECTOR_INTS];
+    if (fread(offsets, 4, SECTOR_INTS, region) != SECTOR_INTS)
       return 1;
     size_t i;
-    for (i = 0; i < 1024; i++)
+    for (i = 0; i < SECTOR_INTS; i++)
       offsets[i] = be32toh(offsets[i]);
-    uint32_t timestamps[1024];
-    if (fread(timestamps, 4, 1024, region) != 1024)
+    uint32_t timestamps[SECTOR_INTS];
+    if (fread(timestamps, 4, SECTOR_INTS, region) != SECTOR_INTS)
       return 1;
-    for (i = 0; i < 1024; i++) {
+    for (i = 0; i < SECTOR_INTS; i++) {
       if (offsets[i] == 0)
         continue;
       uint32_t sectorStart = offsets[i] >> 8;
       uint32_t numSectors = offsets[i] & 0xff;
       if (numSectors == 0)
         continue;
-      if (fseek(region, sectorStart*4096, SEEK_SET) == 0) {
-        unsigned char buf[4096*numSectors];
-        size_t size = fread(buf, 1, sizeof(buf), region);
-        nbt_node* node = nbt_parse_compressed(buf+5, size-5);
-        if (node) {
-          char* dump = nbt_dump_ascii(node);
-          fprintf(stdout, "%s\n", dump);
-          free(dump);
-        } else {
-          fprintf(stderr, "%s\n", nbt_error_to_string(errno));
+      if (fseek(region, sectorStart*SECTOR_BYTES, SEEK_SET) == 0) {
+        unsigned char buf[numSectors*SECTOR_BYTES];
+        if (fread(buf, 1, sizeof(buf), region) == sizeof(buf)) {
+          size_t size = be32toh((uint32_t) buf);
+          nbt_node* node = nbt_parse_compressed(buf+5, size);
+          if (node) {
+            char* dump = nbt_dump_ascii(node);
+            fprintf(stdout, "%s\n", dump);
+            free(dump);
+          } else {
+            fprintf(stderr, "%s\n", nbt_error_to_string(errno));
+          }
+          nbt_free(node);
+          return 0;
         }
-        nbt_free(node);
-        return 0;
       }
     }
-    /*char buf[4096];
-    size_t size = fread(buf, 1, sizeof(buf), region);
-    fprintf(stderr, "size = %lu\n", size);
-    //nbt_node* node = nbt_parse_file(region);
-    nbt_node* node = nbt_parse_compressed(buf, size);
-    if (node) {
-      char* dump = nbt_dump_ascii(node);
-      fprintf(stderr, "%s\n", dump);
-      free(dump);
-    } else {
-      fprintf(stderr, "%s\n", nbt_error_to_string(errno));
-    }*/
   }
   fclose(region);
   return 0;
