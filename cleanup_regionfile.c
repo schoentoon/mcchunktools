a62f3572cd4479e25cabb3c8cc5031d3b54b7ea1
Toon Schoenmakers <nighteyes1993@gmail.com>
2014-01-29 14:37:45 +0100

    Our cleanup regionfile program will now loop to find mca files
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- cleanup_regionfile.c
+++ cleanup_regionfile.c
@@ -21,36 +21,84 @@
 #include <errno.h>
 #include <getopt.h>
 #include <string.h>
+#include <dirent.h>
 
 static const struct option g_LongOpts[] = {
   { "help",       no_argument,       0, 'h' },
-  { "regionfile", required_argument, 0, 'r' },
+  { "world",      required_argument, 0, 'w' },
   { 0, 0, 0, 0 }
 };
 
 int usage(char* program) {
   fprintf(stderr, "Usage: %s [OPTIONS]\n", program);
-  fprintf(stderr, "--regionfile, -r Use this region file [required]\n");
+  fprintf(stderr, "--world, -w Optimize this world [required]\n");
   return 1;
 };
 
+int is_mca(const char* str) {
+  if (str != NULL) {
+    size_t size = strlen(str);
+    if (size >= 4 && str[size-4] == '.' && str[size-3] == 'm' && str[size-2] == 'c' && str[size-1] == 'a')
+      return 1;
+  };
+  return 0;
+};
+
+int loop_directory(const char* dir) {
+  DIR* d = opendir(dir);
+  if (d == NULL)
+    return -1;
+  int output = 0;
+  struct dirent* dp;
+  while ((dp = readdir(d)) != NULL) {
+    if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
+      continue;
+    if (dp->d_type == DT_DIR) {
+      char fullpath[strlen(dir) + strlen(dp->d_name) + 2];
+      if (snprintf(fullpath, sizeof(fullpath), "%s/%s", dir, dp->d_name) > 0)
+        output += loop_directory(fullpath);
+    } else if (dp->d_type == DT_REG && is_mca(dp->d_name) == 1) {
+      char fullpath[strlen(dir) + strlen(dp->d_name) + 2];
+      if (snprintf(fullpath, sizeof(fullpath), "%s/%s", dir, dp->d_name) > 0) {
+        regionfile* region = open_regionfile(fullpath);
+        int cleaned_up = cleanup_region(region);
+        if (cleaned_up > 0)
+          output += cleaned_up;
+        else {
+          fprintf(stderr, "An error occured while processing %s, exiting.\n", fullpath);
+          exit(1);
+        };
+        fprintf(stderr, "%s:%d\n", fullpath, cleaned_up);
+        free_region(region);
+      };
+    };
+  };
+  closedir(d);
+  return output;
+};
+
 int main(int argc, char** argv) {
-  char* rfile = NULL;
-  int arg, optindex;
-  while ((arg = getopt_long(argc, argv, "hr:", g_LongOpts, &optindex)) != -1) {
-    switch (arg) {
-    case 'h':
-      return usage(argv[0]);
-    case 'r':
-      rfile = optarg;
-      break;
+  char* wdir = NULL;
+  {
+    int arg, optindex;
+    while ((arg = getopt_long(argc, argv, "hw:", g_LongOpts, &optindex)) != -1) {
+      switch (arg) {
+      case 'h':
+        return usage(argv[0]);
+      case 'w':
+        wdir = optarg;
+        break;
+      }
     }
   }
-  if (!rfile)
+  if (!wdir)
     return usage(argv[0]);
-  regionfile* region = open_regionfile(rfile);
-  int cleaned_up = cleanup_region(region);
-  fprintf(stderr, "Cleaned up a total of %d bytes in %s\n", cleaned_up, rfile);
-  free_region(region);
+  {
+    size_t len = strlen(wdir);
+    if (wdir[len-1] == '/')
+      wdir[len-1] = '\0';
+  }
+  int cleaned_up = loop_directory(wdir);
+  fprintf(stderr, "Zeroed out a total of %d bytes.\n", cleaned_up);
   return 0;
 };
